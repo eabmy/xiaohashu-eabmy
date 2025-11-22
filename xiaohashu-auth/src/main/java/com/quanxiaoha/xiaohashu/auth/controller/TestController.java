7e68726105d8f8ad6e41685b2e44b2c2f2d68df3
余辉 <yuhui@eamil.com>
2025-11-22 17:00:16 +0800

    4.7 自定义 Jackson 配置：支持 LocalDateTime 日期 API

--- xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/TestController.java
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/TestController.java
@@ -3,6 +3,8 @@ package com.quanxiaoha.xiaohashu.auth.controller;
 import com.quanxiaoha.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.quanxiaoha.framework.common.response.Response;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -29,6 +31,12 @@ public class TestController {
                 .nickName("犬小哈")
                 .createTime(LocalDateTime.now())
                 .build());
+    }
+
+    @PostMapping("/test3")
+    @ApiOperationLog(description = "测试接口3")
+    public Response<User> test2(@RequestBody User user) {
+        return Response.success(user);
     }
 }
 
